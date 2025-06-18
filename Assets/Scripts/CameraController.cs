094f49460f0ba0c5779326b47e72c3f0f99ae3e1
GrafEnters <graf.enters@gmail.com>
2025-06-18 17:17:36 +0200

    follow cam wip

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField] private CinemachineCamera _followCam, _staticCam;
+    [SerializeField] private Transform _defaultTarget;
+    [SerializeField] private LayerMask _droneLayer;
+
+    private void Start() {
+        ChangeCam(false);
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, 100f, _droneLayer)) {
+                ChangeCam(true);
+                _followCam.LookAt = hit.transform;
+                _followCam.Follow = hit.transform;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1)) {
+            ChangeCam(false);
+            _followCam.Follow = _defaultTarget;
+            _followCam.LookAt = _defaultTarget;
+        }
+    }
+
+    private void ChangeCam(bool isFollowing) {
+        _followCam.enabled = isFollowing;
+        _staticCam.enabled = !isFollowing;
+    }
+}
